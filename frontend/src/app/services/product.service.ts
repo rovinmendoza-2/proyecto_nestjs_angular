e4956e4e2b12c99217a056ea5d85b9b9ef93812f
rovinmendoza-2 <137201185+rovinmendoza-2@users.noreply.github.com>
2023-10-09 17:45:29 -0600

    obteniendo producto con su imagen

--- frontend/src/app/services/product.service.ts
+++ frontend/src/app/services/product.service.ts
@@ -6,11 +6,12 @@ import { Injectable } from '@angular/core';
 })
 export class ProductService {
 
-  private apiUrl = '';
+  private apiUrl = 'http://localhost:3000/products';
 
   constructor(private http: HttpClient) { }
 
   getProducts() {
+    console.log(this.apiUrl)
     return this.http.get(this.apiUrl);
   }
 }
